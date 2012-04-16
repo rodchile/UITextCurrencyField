fdd3b374707aeeab9d7f9d5ca3d318400d67fd66
Rodrigo García S <rod@rodrigogarcia.net>
2012-04-16 20:43:03 -0300

    Initial Commit

---
+++ UITextCurrencyView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UITextCurrencyView
+//
+//  Created by Rodrigo Garcia on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
